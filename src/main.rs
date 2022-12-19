1c09451f9dde7faa2839467d7df6bf1aacdb1c6d
Manoj Manivannan <manoj.manivannan@infovista.com>
2022-12-19 17:04:03 +0100

    add test

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+#![allow(unused)]
 use std::io;
 use rand::Rng;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number");
     
-    let secret = rand::thread_rng().gen_range(1..=100);
+    let secret = rand::thread_rng().gen_range(1..=10);
     // println!("The secret number is {secret}");
 
     let mut guess = String::new();
@@ -14,13 +17,19 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
         
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
+    let guess1: u32 = guess.trim().parse().expect("Please type a number");
     
-    println!("You guessed: {guess}");
-    match guess.cmp(&secret) {
+    println!("You guessed: {guess1}");
+    match guess1.cmp(&secret) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too Big!"),
-        Ordering::Equal => println!("Too small!"),
+        Ordering::Equal => println!("MATCH!"),
     }
     
+}
+
+#[test]
+fn test_main() {
+    assert_eq!(1,1);
+    assert_eq!(4,4);
 }
